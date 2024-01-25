4879bc1353a90522c6b3bd20ec7a3ff2c1b6b6d8
sulhanfuadi <138558396+sulhanfuadi@users.noreply.github.com>
2024-01-25 19:41:08 +0700

    quiz 01

---
+++ 2-daftar-inspeksi.py
@@ -0,0 +1,36 @@
+# Kuis Coding: Daftar Inspeksi
+# January 25, 2024
+
+# SOAL
+# Ketika sedang bekerja, Bapak dipanggil oleh rekan kerja untuk melakukan inspeksi di area pembangunan rumah. 
+# Hal ini harus dilakukan pengawas proyek supaya memastikan bahwa pembangunan rumah berjalan dengan baik serta 
+# sesuai dengan rencana dan spesifikasi yang ditetapkan.
+# Ada beberapa hal yang perlu diinspeksi dan dipersiapkan selama proses pembangunan sebagai berikut.
+#  - Pemahaman terhadap rencana dan desain.
+#  - Ketersediaan bahan dan peralatan.
+#  - Pengawasan pekerjaan.
+#  - Kualitas konstruksi.
+#  - Keselamatan kerja.
+#  - Koordinasi dengan pemilik proyek.
+# Bapak Fajar mencatat seluruh inspeksi tersebut supaya mempermudah dalam mengerjakan tugasnya. 
+# Setiap inspeksi tersebut harus dijalani tahap demi tahap tanpa terkecuali. 
+# Dengan kata lain, Bapak Fajar tidak boleh melewatkan atau menambahkan aksi yang sudah terdefinisi sebelumnya.
+# Untuk membantu bapak dalam proses pencatatan, buatlah program sederhana yang mampu 
+# menyimpan daftar inspeksi ke dalam variabel bertipe data list.
+
+# TODO:
+#  1. Buatlah variabel inspectionList bertipe data list untuk menyimpan daftar inspeksi dengan isian sebagai berikut.
+#     - pemahaman terhadap rencana dan desain
+#     - ketersediaan bahan dan peralatan
+#     - pengawasan pekerjaan
+#     - kualitas konstruksi
+#     - keselamatan kerja
+#     - koordinasi dengan pemilik proyek
+
+# SOLUSI:
+inspectionList: list = ["pemahaman terhadap rencana dan desain",
+                        "ketersediaan bahan dan peralatan",
+                        "pengawasan pekerjaan",
+                        "kualitas konstruksi",
+                        "keselamatan kerja",
+                        "koordinasi dengan pemilik proyek"]
